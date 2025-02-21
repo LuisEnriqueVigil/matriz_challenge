627e731e78ed42adc97fb9cf94c61f810695b391
Luis Enrique Vigil <luis.vigil.santillan@bbva.com>
2025-02-21 14:14:45 -0500

    buid matriz functions

--- lib/domain/controllers/home_controller.dart
+++ lib/domain/controllers/home_controller.dart
@@ -1,15 +1,28 @@
 
 import 'package:flutter/widgets.dart';
+import 'dart:math';
 
 class HomeController with ChangeNotifier{
   int _cantValuesOfMatriz  = 0; 
   TextEditingController _textEditingControllerInput = TextEditingController(); 
-  List<List<String>> _matrixOfNumbers = [];
-  List<List<String>> _matrixOfNumbersOutPut = [];
+  List<List<int>> _matrixOfNumbers = [];
+  List<List<int>> _matrixOfNumbersOutPut = [];
 
   int get cantValuesOfMatriz => _cantValuesOfMatriz;
   set cantValuesOfMatriz ( int value){
    _cantValuesOfMatriz = value;
+   notifyListeners();
+  }
+
+  List<List<int>> get matrixOfNumbers => _matrixOfNumbers;
+  set matrixOfNumbers ( List<List<int>> value){
+   _matrixOfNumbers = value;
+   notifyListeners();
+  }
+
+  List<List<int>> get matrixOfNumbersOutPut => _matrixOfNumbersOutPut;
+  set matrixOfNumbersOutPut ( List<List<int>> value){
+   _matrixOfNumbersOutPut = value;
    notifyListeners();
   }
 
@@ -22,7 +35,22 @@ class HomeController with ChangeNotifier{
   void generateMatrix(){
     RegExp regExp = RegExp(r'\[[^\[\]]*\]');
     List<String> valuesOfMatrix= regExp.allMatches(textEditingControllerInput.text).map((match) => match.group(0)!).toList();
-    debugPrint ("values: ${valuesOfMatrix}");
+    RegExp regExpOnlyNumbers = RegExp(r'\d+'); // Busca uno o más dígitos
+    List<int>  valuesOnlyNumbers = regExpOnlyNumbers.allMatches(textEditingControllerInput.text).map((match) => int.parse(match.group(0)!)).toList();
+    debugPrint("vlues int: $valuesOnlyNumbers");
+
+    int n = sqrt(valuesOnlyNumbers.length).toInt();
+    int index = 0; 
+    
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        matrixOfNumbers[i][j] = valuesOnlyNumbers[index++]; // Asigna el producto de fila x columna
+      }
+    }
+
+    for (var row in matrixOfNumbers) {
+      debugPrint(row.toString());
+    }
   }
   
 }
